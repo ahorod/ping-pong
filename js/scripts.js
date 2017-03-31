f8eba4fba8baeacfe6899d1ed7101c86dfb087b8
ahorod <prozerpina112009@gmail.com>
2017-03-31 11:31:18 -0700

    add reverse option

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-var pingPong = function (number){
+var numberArr = [];
+var basic = function (number){
 //display warning if enterd value is not numeric, else create array from 1 to user input
 var numberInd;
+// clears global var before executing function, after button click output is refreshesd
 var numberArr = [];
 if ( $.isNumeric(number) === false){
   alert("Please enter integer number");
@@ -23,23 +25,46 @@ for (i=0; i<numberArr.length; i++){
   else if (numberArr[i]%5 === 0) {
     numberArr[i] = "pong";
   }
+  }
 }
+return numberArr;
+}
+
+var pingPongReverse = function (number) {
+  numberArr = basic(number);
+  numberArr.reverse();
   console.log(numberArr);
 //display result as ul
 for (i=0;i<numberArr.length; i++){
 $("ul#game").append("<li>" + numberArr[i] + "</li>");
 }
 }
+
+var pingPong = function (number){
+  numberArr = basic(number);
+  console.log(numberArr);
+//display result as ul
+for (i=0;i<numberArr.length; i++){
+$("ul#game").append("<li>" + numberArr[i] + "</li>");
+}
 }
 
 $(document).ready(function() {
-
-    $("form#ping-pong").submit(function(event) {
-        event.preventDefault();
-        // clear result before adding new
-        $("#game").html("");
-        var number = parseInt($("#input").val());
-        pingPong(number);
-        $("#result").show();
+  $("button#play").click(function(event) {
+      event.preventDefault();
+      // clear result before adding new
+      $("#game").html("");
+      var number = parseInt($("#input").val());
+      pingPong(number);
+      $("#result").show();
+  });
+    $("button#reverse").click(function(event) {
+      event.preventDefault();
+      // clear result before adding new
+      $("#game").html("");
+      var number = parseInt($("#input").val());
+      pingPongReverse(number);
+      $("#result").show();
     });
+
 });
